5f129efaa8c56f9ef7cc894ce4cde038086fdca6
Pavel Razuvayev <pavel.razuvayev@gmail.com>
2015-04-22 21:49:21 -0500

    Add ful_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | San Antonio Poker Club"
+    assert_select "title", "San Antonio Poker Club"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | San Antonio Poker Club"
+    assert_select "title", "San Antonio Poker Club"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | San Antonio Poker Club"
+    assert_select "title", "San Antonio Poker Club"
   end
 
 end
