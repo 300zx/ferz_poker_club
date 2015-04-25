88dc3a8b7337e23e5e2d715ca30f75a7b45ccb86
Pavel Razuvayev <pavel.razuvayev@gmail.com>
2015-04-24 21:33:09 -0500

    finish layout and routes; add user controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "San Antonio Poker Club"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "San Antonio Poker Club"
+  end
 end
